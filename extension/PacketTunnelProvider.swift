ed0a4a8fb538da1cd95528fe4331a06717d21f7d
hopwesley <ws@sinoc.io>
2022-06-25 17:20:24 +0800

    save tmp

--- extension/PacketTunnelProvider.swift
+++ extension/PacketTunnelProvider.swift
@@ -8,13 +8,12 @@
 
 import NetworkExtension
 import SwiftyJSON
+import Tun2Simple
 
 class PacketTunnelProvider: NEPacketTunnelProvider {
         let httpQueue = DispatchQueue.global(qos: .userInteractive)
         let proxyServerPort :UInt16 = 31080
         let proxyServerAddress = "127.0.0.1";
-        
-//        var pendingStartCompletion: ((Error?) -> Void)?
         
         var golobal = false
         override func startTunnel(options: [String : NSObject]?, completionHandler: @escaping (Error?) -> Void) {
@@ -29,7 +28,13 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
                         try ApiService.pInst.setup(param: ops)
                         let settings = try initSetting()
                         self.golobal = (ops["GLOBAL_MODE"] as? Bool == true)
-                        
+                        var err:NSError? = nil
+                        Tun2SimpleInitApp(self, &err)
+                        if let e = err{
+                                completionHandler(e)
+                                NSLog("--------->startTunnel failed\n[\(e.localizedDescription)]")
+                                return
+                        }
                         self.setTunnelNetworkSettings(settings, completionHandler: {
                                 error in
                                 guard error == nil else{
@@ -37,8 +42,8 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
                                         NSLog("--------->setTunnelNetworkSettings err:\(error!.localizedDescription)")
                                         return
                                 }
-                               
                                 completionHandler(nil)
+                                self.readPackets()
                         })
                         
                 }catch let err{
@@ -50,25 +55,23 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
                 
                 let networkSettings = NEPacketTunnelNetworkSettings.init(tunnelRemoteAddress: proxyServerAddress)
                 networkSettings.mtu = NSNumber.init(value: 1500)
-//                networkSettings.tunnelOverheadBytes = 150
-                
-//                let proxySettings = NEProxySettings()
-//                proxySettings.autoProxyConfigurationEnabled = true
-//                proxySettings.proxyAutoConfigurationJavaScript = Utils.JavaScriptString
-//                proxySettings.matchDomains=[""]
-//                networkSettings.proxySettings = proxySettings;
                 
                 let dnsSettings = NEDNSSettings(servers: ["8.8.8.8", "1.1.1.1"])
-//                let dnsSettings = NEDNSOverHTTPSSettings(servers: ["8.8.8.8", "1.1.1.1"])
-//                let dnsSettings = NEDNSOverHTTPSSettings(servers: ["223.5.5.5","223.6.6.6","2400:3200:baba::1","2400:3200::1"])
-//                dnsSettings.serverURL = URL(string: "https://dns.google/dns-query")
                 dnsSettings.matchDomains = [""]
                 networkSettings.dnsSettings = dnsSettings
                 
                 let ipv4Settings = NEIPv4Settings(addresses: ["10.0.0.8"], subnetMasks: ["255.255.255.0"])
-                ipv4Settings.includedRoutes = [.default()]
+                ipv4Settings.includedRoutes = [NEIPv4Route.default()]
+                ipv4Settings.excludedRoutes = [
+                        NEIPv4Route(destinationAddress: "10.0.0.0", subnetMask: "255.0.0.0"),
+                        NEIPv4Route(destinationAddress: "100.64.0.0", subnetMask: "255.192.0.0"),
+                        NEIPv4Route(destinationAddress: "127.0.0.0", subnetMask: "255.0.0.0"),
+                        NEIPv4Route(destinationAddress: "169.254.0.0", subnetMask: "255.255.0.0"),
+                        NEIPv4Route(destinationAddress: "172.16.0.0", subnetMask: "255.240.0.0"),
+                        NEIPv4Route(destinationAddress: "192.168.0.0", subnetMask: "255.255.0.0"),
+                        NEIPv4Route(destinationAddress: "17.0.0.0", subnetMask: "255.0.0.0"),
+                ]
                 networkSettings.ipv4Settings = ipv4Settings;
-                
                 return networkSettings
         }
         
@@ -112,44 +115,47 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
         }
 }
 
-
-
-//                proxySettings.httpEnabled = true;
-//                proxySettings.httpServer = NEProxyServer.init(address: proxyServerAddress, port: Int(proxyServerPort))
-//                proxySettings.httpsEnabled = true;
-//                proxySettings.httpsServer = NEProxyServer.init(address: proxyServerAddress, port: Int(proxyServerPort))
-
-//                        ipv4Settings.includedRoutes = [NEIPv4Route.default()]
-//                ipv4Settings.excludedRoutes = [
-//                        NEIPv4Route(destinationAddress: "10.0.0.0", subnetMask: "255.0.0.0"),
-//                        NEIPv4Route(destinationAddress: "100.64.0.0", subnetMask: "255.192.0.0"),
-//                        NEIPv4Route(destinationAddress: "127.0.0.0", subnetMask: "255.0.0.0"),
-//                        NEIPv4Route(destinationAddress: "169.254.0.0", subnetMask: "255.255.0.0"),
-//                        NEIPv4Route(destinationAddress: "172.16.0.0", subnetMask: "255.240.0.0"),
-//                        NEIPv4Route(destinationAddress: "192.168.0.0", subnetMask: "255.255.0.0"),
-//                        NEIPv4Route(destinationAddress: "17.0.0.0", subnetMask: "255.0.0.0"),
-//                    ]
-//
-
-//                let ipv4Settings = NEIPv4Settings(addresses: ["10.0.0.8"], subnetMasks: ["255.255.255.0"])
-//                var includedRoutes = [NEIPv4Route]()
-//                includedRoutes.append(NEIPv4Route(destinationAddress: "74.125.0.0", subnetMask: "255.255.0.0"))
-//                ipv4Settings.includedRoutes = includedRoutes
-//
-//                networkSettings.ipv4Settings = ipv4Settings;
-
-/*
- 
- 64.18.0.0/20
- 64.233.160.0/19
- 66.102.0.0/20
- 66.249.80.0/20
- 72.14.192.0/18
- 74.125.0.0/16
- 173.194.0.0/16
- 207.126.144.0/20
- 209.85.128.0/17
- 216.58.208.0/20
- 216.239.32.0/19
- 
- */
+extension PacketTunnelProvider:Tun2SimpleDeviceIProtocol{
+        func log(_ s: String?) {
+                guard let log = s else{
+                        return
+                }
+                NSLog("-------->\(log)")
+        }
+        
+        func stackClosed() {
+                self.exit()
+        }
+        
+        private func exit(){
+                Darwin.exit(EXIT_SUCCESS)
+        }
+        func stack2Dev(_ data: Data?) {
+                guard let d = data else{
+                        NSLog("-------->stack2Dev with empty data......")
+                        self.exit()
+                        return
+                }
+                
+                let packet = NEPacket(data: d, protocolFamily: sa_family_t(AF_INET))
+                packetFlow.writePacketObjects([packet])
+        }
+        
+        private func readPackets() {
+                NSLog("--------->start to read packets......")
+                packetFlow.readPacketObjects { packets in
+                        var err:NSError? = nil
+                        var no:Int = 0
+                        for p in packets{
+                                Tun2SimpleInputDevData(p.data, &no, &err)
+                                if err != nil{
+                                        NSLog("-------->Tun2SimpleInputDevData err[\(err!.localizedDescription)]......")
+                                        self.exit()
+                                        return
+                                }
+                        }
+                        self.readPackets()
+                }
+        }
+        
+}
